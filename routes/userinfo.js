34e5c20bb646b8ff34b14e9b8efe85600fc386b4
Jared Hanson <jaredhanson@gmail.com>
2022-04-24 07:58:55 -0700

    Add scope and error handling to userinfo endpoint.

--- routes/userinfo.js
+++ routes/userinfo.js
@@ -1,3 +1,4 @@
+var createError = require('http-errors');
 var express = require('express');
 var passport = require('passport');
 var HTTPBearerStrategy = require('passport-http-bearer');
@@ -18,25 +19,39 @@ passport.use(new HTTPBearerStrategy(function verify(token, cb) {
   var user = {
     id: parseInt(jwt.payload.sub)
   };
-  // TODO: Pass scope as info
-  return cb(null, user);
+  var authInfo = {
+    scope: jwt.payload.scope ? jwt.payload.scope.split(' ') : []
+  };
+  return cb(null, user, authInfo);
 }));
 
 
 var router = express.Router();
 
-router.get('/userinfo', passport.authenticate('bearer', { session: false }), function(req, res, next) {
+router.get('/userinfo', passport.authenticate('bearer', { session: false, failWithError: true }), function(req, res, next) {
   db.get('SELECT * FROM users WHERE id = ?', [ req.user.id ], function(err, row) {
     if (err) { return next(err); }
-    // TODO: Handle undefined row.
+    if (!row) { return next(createError(403)); }
     var info = {
       sub: row.id.toString()
     };
-    // TODO: check scope
-    if (row.name) { info.name = row.name; }
-    if (row.username) { info.preferred_username = row.username; }
+    if (req.authInfo.scope.indexOf('profile') != -1) {
+      if (row.name) { info.name = row.name; }
+      if (row.username) { info.preferred_username = row.username; }
+    }
+    if (req.authInfo.scope.indexOf('email') != -1) {
+      if (row.email) { info.email = row.email; }
+      if (row.email_verified) { info.email_verified = row.email_verified; }
+    }
+    if (req.authInfo.scope.indexOf('phone') != -1) {
+      if (row.phone_number) { info.phone_number = row.phone_number; }
+      if (row.phone_number_verified) { info.phone_number_verified = row.phone_number_verified; }
+    }
     res.json(info);
   });
+}, function(err, req, res, next) {
+  res.status(err.status || 500);
+  return res.end();
 });
 
 module.exports = router;
